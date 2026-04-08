89896a6df8df4d94d039f82f3bd60d832f5a56e5
noutatoum <124636771+noutatoum@users.noreply.github.com>
2026-04-08 10:39:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="FRONTIER SCAN v1.2", layout="centered")
+st.set_page_config(page_title="FRONTIER SCAN", layout="centered")
 
 frontier_html = """
 <!DOCTYPE html>
